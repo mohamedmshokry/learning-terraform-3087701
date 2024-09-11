967a65fad10a0761735570b7483d3b3aed29bb4e
Mohamed Magdy Shokry <mohamed.magdyshokry@gmail.com>
2024-09-11 09:53:27 +0300

    Use module security group

--- main.tf
+++ main.tf
@@ -1,67 +1,82 @@
-# data "aws_ami" "app_ami" {
-#   most_recent = true
+data "aws_ami" "app_ami" {
+  most_recent = true
 
-#   filter {
-#     name   = "name"
-#     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-#   }
+  filter {
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-#   owners = ["979382823631"] # Bitnami
-# }
+  owners = ["979382823631"] # Bitnami
+}
 
-# data "aws_vpc" "default" {
-#   default = true
-# }
+data "aws_vpc" "default" {
+  default = true
+}
 
-# resource "aws_instance" "blog" {
-#   ami           = data.aws_ami.app_ami.id
-#   instance_type = var.instance_type
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 
-#   vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-#   tags = {
-#     Name = "HelloWorld"
-#   }
-# }
+  tags = {
+    Name = "HelloWorld"
+  }
+}
 
-# resource "aws_security_group" "blog" {
-#   name        = "blog"
-#   description = "Allow http and https in. Allow everything out"
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.2.0"
 
-#   vpc_id = data.aws_vpc.default.id
-# }
+  name = "blog_new"
 
-# resource "aws_security_group_rule" "blog_http_in" {
-#   type        = "ingress"
-#   from_port   = 80
-#   to_port     = 80
-#   protocol    = "tcp"
-#   cidr_blocks = ["0.0.0.0/0"]
+  vpc_id = data.aws_vpc.default.id
+  
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
-#   security_group_id = aws_security_group.blog.id
-# }
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+}
 
-# resource "aws_security_group_rule" "blog_https_in" {
-#   type        = "ingress"
-#   from_port   = 443
-#   to_port     = 443
-#   protocol    = "tcp"
-#   cidr_blocks = ["0.0.0.0/0"]
+resource "aws_security_group" "blog" {
+  name        = "blog"
+  description = "Allow http and https in. Allow everything out"
 
-#   security_group_id = aws_security_group.blog.id
-# }
+  vpc_id = data.aws_vpc.default.id
+}
 
-# resource "aws_security_group_rule" "blog_everything_out" {
-#   type        = "egress"
-#   from_port   = 0
-#   to_port     = 0
-#   protocol    = "-1"
-#   cidr_blocks = ["0.0.0.0/0"]
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 
-#   security_group_id = aws_security_group.blog.id
-# }
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
